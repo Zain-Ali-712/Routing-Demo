dfb1cea93c840447f8374a16336ea58bc1118deb
Zain-Ali-712 <za9690000@gmail.com>
2025-07-19 12:17:17 +0500

    Headers and cookies done

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,18 @@
-export async function GET(){
-    return new Response("Api Profile data");
-}
+import type { NextRequest } from "next/server";
+import { headers,cookies } from "next/headers";
+
+export async function GET( request: NextRequest){
+    const headerList = await headers();
+    const cookieList = await cookies();
+    const theme = request.cookies.get("theme");
+    console.log(theme);
+    cookieList.set("resultPerPage","20");
+    console.log(cookieList.get("resultPerPage"));
+    console.log(headerList.get("Authorization"));
+    return new Response("<h1>Api Profile data</h1>",{
+        headers:{
+            "Content-Type":"text/html",
+            "Set-Cookie":"theme=Dark",
+        },
+    });
+} 
